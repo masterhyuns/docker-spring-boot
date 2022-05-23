29fc687654a0c1c4d770345d34aa2f39af59817f
김태현 <kth@elonsoft.co.kr>
2022-05-23 15:27:50 +0900

    first commit

---
+++ src/test/java/com/example/job/jibdocker/JibDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job.jibdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
